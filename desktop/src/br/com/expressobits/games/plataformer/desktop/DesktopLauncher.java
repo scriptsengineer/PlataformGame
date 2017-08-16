b56d38a42360a34c0f5949e359e35cae7e9ddeab
Rafael Correa <rafinha2108@gmail.com>
2017-08-16 12:07:08 -0300

    Initial commit

---
+++ desktop/src/br/com/expressobits/games/plataformer/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package br.com.expressobits.games.plataformer.desktop;
+
+import br.com.expressobits.games.plataformer.Plataformer;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280,720);
+		config.setResizable(false);
+		config.useVsync(true);
+		new Lwjgl3Application(new Plataformer(), config);
+	}
+}
